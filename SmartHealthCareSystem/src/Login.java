c47215faaa93a3192c58dd2b76934da6f9f80342
saahil20 <saahil18105@iiitd.ac.in>
2018-10-28 17:13:40 +0530

    First Commit

---
+++ SmartHealthCareSystem/src/Login.java
@@ -0,0 +1,81 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JRadioButton;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+
+public class Login {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login window = new Login();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Login() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 640, 480);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(217, 104, 189, 22);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(217, 152, 189, 22);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JRadioButton rdbtnPatient = new JRadioButton("Patient");
+		rdbtnPatient.setBounds(217, 197, 77, 25);
+		frame.getContentPane().add(rdbtnPatient);
+		
+		JRadioButton rdbtnDoctor = new JRadioButton("Doctor");
+		rdbtnDoctor.setBounds(329, 197, 77, 25);
+		frame.getContentPane().add(rdbtnDoctor);
+		
+		JRadioButton rdbtnAdmin = new JRadioButton("Admin");
+		rdbtnAdmin.setBounds(217, 236, 77, 25);
+		frame.getContentPane().add(rdbtnAdmin);
+		
+		JLabel lblUsername = new JLabel("UserName");
+		lblUsername.setBounds(92, 107, 83, 16);
+		frame.getContentPane().add(lblUsername);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setBounds(92, 155, 56, 16);
+		frame.getContentPane().add(lblPassword);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.setBounds(217, 294, 189, 25);
+		frame.getContentPane().add(btnNewButton);
+	}
+}
